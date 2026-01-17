26281cdc2593c7da0c2e783214fe689a10624a12
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2026-01-17 20:20:04 +0530

    Create sort colors.java

---
+++ arrays/sort colors.java
@@ -0,0 +1,35 @@
+//straight forward solution using counting sort
+class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                red++;
+            } else if(nums[i] == 1) {
+                white++;
+            } else {
+                blue++;
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            while(red > 0) {
+                nums[i] = 0;
+                i++;
+                red--;
+            }
+            while(white > 0) {
+                nums[i] = 1;
+                i++;
+                white--;
+            }
+             while(blue > 0) {
+                nums[i] = 2;
+                i++;
+                blue--;
+            }
+        }
+    }
+}
