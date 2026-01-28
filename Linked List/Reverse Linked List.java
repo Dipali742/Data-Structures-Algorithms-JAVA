a40b92a20aa00bc5ac11341c7361c63795304c58
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2026-01-28 11:09:11 +0530

    Update Reverse Linked List.java

--- Linked List/Reverse Linked List.java
+++ Linked List/Reverse Linked List.java
@@ -53,3 +53,31 @@ class Solution {
 }
 
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+   public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+       return recursive(prev, curr);
+    }
+
+    public ListNode recursive(ListNode prev, ListNode curr) {
+        if(curr == null)
+            return prev;
+        ListNode temp = curr.next;
+        curr.next = prev;
+        return recursive(curr, temp);
+    }
+}
